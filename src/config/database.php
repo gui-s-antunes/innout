9fa03ea43ede2bb0a4ca0ae7563a6838505943e3
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-11-15 14:53:58 -0300

    database file to connect to a database is done - mysqli

---
+++ src/config/database.php
@@ -0,0 +1,12 @@
+<?php
+class Database{
+    public static function getConnection(){
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath);
+        $connection = new mysqli($env['host'], $env['username'], $env['password'], $env['database']);
+        if($connection->connect_error){
+            die("Erro: " . $connection->connect_error);
+        }
+        return $connection;
+    }
+}
