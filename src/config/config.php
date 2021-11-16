388936ffedb8387d22dec465428c051697ebcd98
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-11-16 14:36:42 -0300

    config fixado erro de timezone

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-date_default_timezone_get('America/Sao_Paulo');
+date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8', 'portuguese');
 
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models')); // constantes das pastas do projeto
