7ed3deadf6f5fc3ea281bdf04c38980fd63d4793
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-11-16 15:14:01 -0300

    implementacao do select feito do model

--- src/models/Model.php
+++ src/models/Model.php
@@ -1,7 +1,7 @@
 <?php
 
 class Model {
-    protected static $tableName = '';
+    protected static $tableName = 'users';
     protected static $columns = [];
     protected $values = [];
 
@@ -24,5 +24,34 @@ class Model {
 
     public function __set($key, $value){
         $this->values[$key] = $value;
+    }
+
+    public static function getSelect($filters = [], $columns = '*'){
+        $sql = "SELECT ${columns} FROM " . static::$tableName . static::getFilters($filters);
+        echo 'Valor de coluna: ' . $columns . '<br>';
+        return $sql;
+    }
+
+    private static function getFilters($filters){
+        $sql = '';
+        if(count($filters) > 0){
+            $sql .= ' WHERE 1 = 1';
+            foreach($filters as $colunm => $value){
+                $sql .= " AND ${colunm} = " . static::getFormatedValue($value);
+            }
+        }
+        return $sql;
+    }
+
+    private static function getFormatedValue($value){
+        if(is_null($value)){
+            return 'null';
+        }
+        elseif(gettype($value) === 'string'){
+            return "'${value}'"; // sendo string precisa de aspas quando passar o código sql. Ex: WHERE name = 'raphael';
+        }
+        else{
+            return $value;
+        }
     }
 }
