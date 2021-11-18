c393a46f2030388791b43963138c77e87d403ccd
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-11-18 17:53:47 -0300

    index.php with config and login controller yet

--- public/index.php
+++ public/index.php
@@ -1,13 +1,123 @@
-<h1>HEHE BOI :)</h1>
+<!-- <h1>HEHE BOI :)2</h1> -->
 
 <?php
-require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+// ini_set('display_errors',1);
+// ini_set('display_startup_erros',1);
+// error_reporting(E_ALL);
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(CONTROLLER_PATH . '/login.php');
+// loadView('login', ['piece' => 'roger']);
 
-$sql = "SELECT * FROM users";
-$result = Database::getResultFromQuery($sql);
 
-while($row = $result->fetch_assoc()){
-    print_r($row);
-    echo '<br>';
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require_once(VIEW_PATH . '/login.php');
+// require_once(MODEL_PATH . '/Login.php');
+
+// $login = New Login([
+//     'email' => 'admin@cod3r.com.br',
+//     'password' => 'a'
+// ]);
+
+// try{
+//     $login->checkLogin();
+//     echo 'Tudo certo!';
+// }
+// catch(Exception $e){
+//     echo 'Problema...';
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+
+
+// exemplo de implmementação abaixo
+// $usuario = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']); // User Object ( [values:protected] => Array ( [name] => Lucas [email] => lucas@cod3r.com.br ) )
+// print_r($usuario); // User Object ( [values:protected] => Array ( [name] => Lucas [email] => lucas@cod3r.com.br ) )
+
+// // usando os métodos mágicos de get e set, eles agora não precisam de um função própria, usando a reação do php
+// $usuario->email = 'novoemaildelucas.com';
+// echo '<br>';
+// print_r($usuario); // User Object ( [values:protected] => Array ( [name] => Lucas [email] => novoemaildelucas.com ) )
+// echo '<br>' . $usuario->email; // novoemaildelucas.com
+
+// echo '<br>Fim';
+
+// echo $usuario->getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br'], 'id, name');
+// echo '<br>';
+// echo User::getSelect(['name' => 'gui', 'email' => 'gui@email.com'], 'id, name');
+
+// print_r(User::get(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br'], 'name, email'));
+
+// echo '<br>fim';
+
+
+
+// $sql = "SELECT * FROM users";
+// $result = Database::getResultFromQuery($sql);
+
+// while($row = $result->fetch_assoc()){
+//     print_r($row);
+//     echo '<br>';
+// }
 
