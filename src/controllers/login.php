4efe87d33a48adf5cd25945772f79d9ae41707bb
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-11-18 17:52:37 -0300

    login controller

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'controller login chegou...';
+loadModel('Login');
+
+if(count($_POST) > 0){
+    $login = new Login($_POST);
+    try{
+        $user = $login->checkLogin();
+        echo "Usuário {$user->name} ok!";
+    }
+    catch(Exception $e){
+        echo 'Login falhou :c';
+    }
+}
+
+loadView('login', $_POST);
--- src/views/login.php
+++ src/views/login.php
@@ -23,7 +23,7 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label for="email">E-mail</label>
-                        <input type="text" name='email' id="email" class="form-control">
+                        <input type="text" name='email' id="email" class="form-control" value="<?= $email ?>">
                     </div>
                     <div class="form-group">
                         <label for="password">Senha</label>
