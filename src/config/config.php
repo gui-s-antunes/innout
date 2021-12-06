382fbaff00d6d32d57c5a52d24d3690084dc3fac
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-12-06 14:26:51 -0300

    gerador de tempo, massa de dados que simula o ponto batido do dia

--- src/config/config.php
+++ src/config/config.php
@@ -3,12 +3,17 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8', 'portuguese');
 
+// constantes gerais
+define('DAILY_TIME', 60 * 60 * 8);
+
+// pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models')); // constantes das pastas do projeto
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
 define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers'));
 define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 
+// arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php')); // será usado por todo o projeto
 require_once(realpath(dirname(__FILE__) . '/loader.php'));
 require_once(realpath(dirname(__FILE__) . '/session.php'));
