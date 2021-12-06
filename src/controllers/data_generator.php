d837a72c3807db40d6cb5a40211b8a61598ebc90
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-12-06 15:58:54 -0300

    implementado para resetar banco e inserir automaticamente batimentos de pontos como testes

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -1,4 +1,14 @@
 <?php
+// ini_set('display_errors',1);
+// ini_set('display_startup_erros',1);
+// error_reporting(E_ALL);
+// este arquivo é para testes
+
+loadModel('WorkingHours'); // data_generator é um arquivo de testes, portanto colocaremos diretamente aqui o carregamento
+
+// reset db
+Database::executeSQL('DELETE FROM working_hours');
+Database::executeSQL('DELETE FROM users WHERE id > 5');
 
 function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate){
     $regularDayTemplate = [
@@ -36,4 +46,26 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate){
 
 }
 
-print_r(getDayTemplateByOdds(34, 33, 33));
+function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate){
+    $currentDate = $initialDate;
+    $today = new Datetime();
+    $columns = ['user_id' => $userId, 'work_date' => $currentDate];
+
+    while(isBefore($currentDate, $today)){
+        if(!isWeekend($currentDate)){
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+            $columns = array_merge($columns, $template);
+            $workingHours = new WorkingHours($columns);
+            $workingHours->save();
+        }
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+        $columns['work_date'] = $currentDate;
+    }
+}
+
+$lastMonth = strtotime('first day of last month');
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
+populateWorkingHours(4, date('Y-m-d', $lastMonth), 10, 20, 70);
+
+echo 'fine';
