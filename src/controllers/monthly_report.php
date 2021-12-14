9613d35847cd0bc069798f038a294f379b4d36bf
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-12-14 16:08:04 -0300

    calculo de balanco de horas a partir da expectiva de horas do mes

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -6,6 +6,35 @@ $user = $_SESSION['user'];
 
 $registries = WorkingHours::getMonthlyReport($user->id, new Datetime());
 
+$currentDate = new DateTime();
+$report = [];
+$workDay = 0; // numero de dias do mes comerciais
+$sumOfWorkedTime = 0; // somar horas trabalhadas
+$lastDay = getLastDayOfMonth($currentDate)->format('d'); // último dia do mês atual
+
+for($day = 1; $day <= $lastDay; $day++){
+    $date = $currentDate->format('Y-m') . '-' . sprintf('%02d', $day);
+    $registry = $registries[$date];
+
+    if(isPastWorkDay($date)) $workDay++;
+
+    if($registry){
+        $sumOfWorkedTime += $registry->worked_time;
+        array_push($report, $registry);
+    } else {
+        array_push($report, new WorkingHours([
+            'work_date' => $date,
+            'worked_time' => 0
+        ]));
+    }
+}
+
+$expectedTime = $workDay * DAILY_TIME;
+$balance = getTimeStringFromSeconds(abs($sumOfWorkedTime - $expectedTime));
+$sign = ($sumOfWorkedTime >= $expectedTime) ? '+' : '-';
+
 loadTemplateView('monthly_report', [
-    'registries' => $registries
+    'report' => $report,
+    'sumOfWorkedTime' => $sumOfWorkedTime,
+    'balance' => "{$sign}{$balance}"
 ]);
