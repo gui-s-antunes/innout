d4a9175c3f75b2807bd097a019aea7603e005c9d
Gulherme Antunes <guilherme.silva.antunes1999@gmail.com>
2021-12-15 16:18:37 -0300

    selecionar usuario no relatorio de batimento de horas

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -5,26 +5,32 @@ requireValidSession();
 $currentDate = new DateTime();
 
 $user = $_SESSION['user'];
+$selectedUserId = $user->id;
+$users = null;
+if($user->is_admin){
+    $users = User::get();
+    $selectedUserId = $_POST['user'] ? $_POST['user'] : $user->id;
+}
 
 $selectedPeriod = $_POST['period'] ? $_POST['period'] : $currentDate->format('Y-m');
 $periods = [];
-for($yearDiff = 2; $yearDiff >= 0; $yearDiff--){
+for($yearDiff = 0; $yearDiff <= 2; $yearDiff++){
     $year = date('Y') - $yearDiff;
-    for($month = 1; $month <= 12; $month++){
+    for($month = 12; $month >= 1; $month--){
         $date = new DateTime("{$year}-{$month}-1");
         $periods[$date->format('Y-m')] = strftime('%B de %Y', $date->getTimestamp());
     }
 }
 
-$registries = WorkingHours::getMonthlyReport($user->id, new Datetime());
+$registries = WorkingHours::getMonthlyReport($selectedUserId, $selectedPeriod);
 
 $report = [];
 $workDay = 0; // numero de dias do mes comerciais
 $sumOfWorkedTime = 0; // somar horas trabalhadas
-$lastDay = getLastDayOfMonth($currentDate)->format('d'); // último dia do mês atual
+$lastDay = getLastDayOfMonth($selectedPeriod)->format('d'); // último dia do mês atual
 
 for($day = 1; $day <= $lastDay; $day++){
-    $date = $currentDate->format('Y-m') . '-' . sprintf('%02d', $day);
+    $date = $selectedPeriod . '-' . sprintf('%02d', $day);
     $registry = $registries[$date];
 
     if(isPastWorkDay($date)) $workDay++;
@@ -48,5 +54,8 @@ loadTemplateView('monthly_report', [
     'report' => $report,
     'sumOfWorkedTime' => getTimeStringFromSeconds($sumOfWorkedTime),
     'balance' => "{$sign}{$balance}",
-    'periods' => $periods
+    'selectedPeriod' => $selectedPeriod,
+    'periods' => $periods,
+    'selectedUserId' => $selectedUserId,
+    'users' => $users
 ]);
